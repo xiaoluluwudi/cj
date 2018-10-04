7b86079857dc9351f37bc185255f05372c355ea4
xiaoluluwudi <994813743@qq.com>
2018-10-04 16:40:35 +0800

    ver0.7.7
        1.新增了输入限制(数字)
        2.修正了部分显示错误
        3.修正了修改用户信息的错误
        4.新增了抽奖奖品设置

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 #define s_c sizeof(char)
 #define n_name 10
-#define min_n 0
 #define max_n 100
 #define id_len 15
 #define n_illegal 9
@@ -20,11 +19,15 @@ FILE* inf_prz;      //奖品记录文本
 FILE* inf_user;     //用户文档指针
 FILE* IDs;          //用户ID储存文本
 
+int num = 0;
+char answer[2] = "0";
 char line[6]="=====";       //固定显示
 char start[2] = "{";        //信息起始符
 char over[2] = "}";         //信息终止符
 char id_sign[2] = "&";      //信息起始符
 char name_sign[2] = "(";    //信息起始符
+char prz_m_s[2] = "[";
+char prz_m_e[2] = "]";
 char illegal_word[n_illegal] = "&({}[]\\/";     //禁止输入的非法字符
 char path[10] = "user_inf/";
 
@@ -41,11 +44,15 @@ void User_delete();         //删除用户
 void User_info_c();         //更换用户信息
 void Info_set();            //抽奖信息管理
 void Lottery_set();         //奖品设置
+void Lottery_set_s();       //单组抽奖奖品
+void Lottery_set_m();       //多组抽奖奖品
 void History_set();         //历史信息管理
 void Info();                //信息查看
 void Info_User();           //用户信息查看
 void Info_introduce();      //抽奖简介信息查看
 void Info_prize();          //奖品信息查看
+void Info_prz_s();
+void Info_prz_m();
 void Info_history();        //历史信息查看
 void About_Us();            //关于我们
 int Delete_ID(char *ID);        //删除ID
@@ -58,6 +65,7 @@ void Record_Time(FILE *file);           //记录时间
 void F_Add(char *f,char *b,char *t);
 int Input(char *input, int len);
 int Input_B(char* input, int len);
+int Input_N(char *input);
 
 int main()
 {
@@ -74,19 +82,19 @@ void Instructions()
     cout << "2.后台管理" << endl;
     cout << "3.信息查看" << endl;
     cout << "4.退出" << endl;
+    cout << line << endl;
 }
 
 int MainPage()
 {
-    char answer = '0';
     while(1){
-        cin >> answer;
-        switch(answer-'0'){
+        num = Input_N(answer);
+        switch(num){
         case 1: Lottery(); break;
         case 2: Back_Stage(); break;
         case 3: Info(); break;
         case 4: exit(0); break;
-        default : answer = '0'; break;
+        default : num = 0; break;
         }
         Instructions();
     }
@@ -94,7 +102,6 @@ int MainPage()
 
 void Back_Stage()
 {
-    char answer = '0';
     while(1){
         clrscr();
         cout << line <<"欢迎来到后台" << line << endl;
@@ -103,16 +110,15 @@ void Back_Stage()
         cout << "3.删除抽奖历史信息" << endl;
         cout << "4.用户管理" << endl;
         cout << "5.返回" << endl;
-        cin >> answer;
-        switch(answer-'0'){
+        num = Input_N(answer);
+        switch(num){
             case 1: Info_set(); break;
             case 2: Lottery_set(); break;
             case 3: History_set(); break;
             case 4: User_set(); break;
             case 5: return;
-            default: answer = '0'; break;
+            default: num = 0; break;
         }
-        answer = '0';
     }
 }
 
@@ -128,7 +134,6 @@ void Info_set()
     cout << line << "设置信息简介" << line << endl;
     cout << "编辑抽奖信息简介：" << endl;
     if(Input_B(inf,max_n+1)==0) return;
-    //cin >> inf;
     fwrite(inf,s_c,strlen(inf),inf_itdc);
     fclose(inf_itdc);
     cout << "设置成功！" << endl;
@@ -146,21 +151,40 @@ void History_set()
     fclose(inf_hstr);
     cout << "清除抽奖历史信息？" << endl;
     cout << "1.确认" << endl << "2.返回" << endl;
-    char answer = '0';
+
     while(1){
-        cin>>answer;
-        if((answer-'0') == 1) {
+        num = Input_N(answer);
+        if(num == 1) {
             remove("sys_inf/inf_hstr.txt");
             cout << "删除成功！" <<endl;
             hitback();
             return;
         }
-        else if((answer-'0')==2) return;
-        else answer = '0';
+        else if(num == 2) return;
+        else num = 0;
     }
 }
 
 void Lottery_set()
+{
+      while(1){
+        clrscr();
+        cout << line <<"奖品设置" << line << endl;
+        cout << "1.设置单组抽奖奖品" << endl;
+        cout << "2.设置多组抽奖奖品" << endl;
+        cout << "3.退出" << endl;
+
+        num = Input_N(answer);
+        switch(num){
+            case 1: Lottery_set_s(); break;
+            case 2: Lottery_set_m(); break;
+            case 3: return;
+            default: num = 0; break;
+        }
+    }
+}
+
+void Lottery_set_s()
 {
     clrscr();
     if((inf_prz = fopen("sys_inf/inf_prz.txt","w")) == NULL){
@@ -174,11 +198,11 @@ void Lottery_set()
     int num_p = 0;
     int i = 0;
 
-    cout << line << "设置奖项" << line << endl;
+    cout << line << "设置单组抽奖奖项" << line << endl;
 
-    while(num_t<=0||num_t>99){
-        cout << "奖品种类数(1-99)：";
-        cin >>num_t;
+    while(num_t<=0||num_t>9){
+        cout << "奖品种类数(1-9)：";
+        num_t = Input_N(answer);
     }
     itoa(num_t,N,10);       //数字转字符
     fwrite(N,s_c,strlen(N),inf_prz);
@@ -191,7 +215,7 @@ void Lottery_set()
         num_p = 0;
         while(num_p <=0){
             cout << "数量：" ;
-            cin >> num_p;
+            num_p = Input_N(answer);
         }
 
         cout << "奖品详情：" ;
@@ -212,9 +236,76 @@ void Lottery_set()
     hitback();
 }
 
+void Lottery_set_m()
+{
+    clrscr();
+    if((inf_prz = fopen("sys_inf/inf_prz_m.txt","w")) == NULL){
+        cout << "打开文件失败" << endl;
+        return;
+    }
+    char name[n_name];
+    char N[3];
+    char inf[max_n+1];
+    int num_n = 0;
+    int num_t = 0;
+    int num_p = 0;
+    int i = 0;
+    int j = 0;
+
+    cout << line << "设置多组抽奖奖项" << line << endl;
+
+    while(num_n<=0||num_n>=9){
+        cout << "奖品组数：" ;
+        num_n = Input_N(answer);
+    }
+    itoa(num_n,N,10);
+    fwrite(N,s_c,strlen(N),inf_prz);
+
+    for(i=0;i<num_n;i++){
+        cout <<line<< "第" << i+1 <<"组奖品"<< endl;
+        num_t = 0;
+        while(num_t<=0||num_t>9){
+            cout << "奖品种类数(1-9)：";
+            num_t = Input_N(answer);
+        }
+        Blank(inf_prz);
+        fwrite(prz_m_s,s_c,1,inf_prz);
+        itoa(num_t,N,10);       //数字转字符
+        fwrite(N,s_c,strlen(N),inf_prz);
+        Blank(inf_prz);
+
+        for(j=0;j<num_t;j++){
+            cout << j+1 << "号奖品："<< endl;
+            cout << "名称：" ;
+            if(Input_B(name,n_name) == 0) return;
+            num_p = 0;
+            while(num_p <=0){
+                cout << "数量：" ;
+                num_p = Input_N(answer);
+            }
+
+            cout << "奖品详情：" ;
+            if(Input_B(inf,max_n+1) == 0) return;
+            itoa(num_p,N,10);
+            fwrite(start,s_c,1,inf_prz);
+            fwrite(N,s_c,strlen(N),inf_prz);
+            Blank(inf_prz);
+            fwrite(name_sign,s_c,1,inf_prz);
+            fwrite(name,s_c,strlen(name),inf_prz);
+            Blank(inf_prz);
+            fwrite(inf,s_c,strlen(inf),inf_prz);
+            Blank(inf_prz);
+            fwrite(over,s_c,1,inf_prz);
+            fwrite(prz_m_e,s_c,1,inf_prz);
+        }
+    }
+    fclose(inf_prz);
+    cout << "设置成功！" << endl;
+    hitback();
+}
+
 void Info()
 {
-    char answer='0';
     while(1){
         clrscr();
         cout << line << "信息查看" << line << endl;
@@ -224,15 +315,15 @@ void Info()
         cout << "4.用户信息" << endl;
         cout << "5.关于我们" << endl;
         cout << "6.返回" << endl;
-        cin >> answer;
-        switch(answer-'0'){
+        num = Input_N(answer);
+        switch(num){
             case 1: Info_introduce(); break;
             case 2: Info_prize(); break;
             case 3: Info_history(); break;
             case 4: Info_User(); break;
             case 5: About_Us(); break;
             case 6: return;
-            default: answer = '0';break;
+            default: num = 0;break;
         }
     }
 }
@@ -257,6 +348,24 @@ void Info_introduce()
 
 void Info_prize()
 {
+    while(1){
+        clrscr();
+        cout << line <<"奖品信息查看" << line << endl;
+        cout << "1.查看单组抽奖奖品信息" << endl;
+        cout << "2.查看多组抽奖奖品信息" << endl;
+        cout << "3.返回" << endl;
+        num = Input_N(answer);
+        switch(num){
+            case 1: Info_prz_s(); break;
+            case 2: Info_prz_m(); break;
+            case 3: return;
+            default: num = 0; break;
+        }
+    }
+}
+
+void Info_prz_s()
+{
     clrscr();
     if((inf_prz = fopen("sys_inf/inf_prz.txt","r")) == NULL){
         cout << "读取信息失败" << endl;
@@ -264,11 +373,11 @@ void Info_prize()
         return;
     }
     char word = fgetc(inf_prz);
-    int num = word - '0';
+    int n_t = word - '0';
     int i = 0;
 
     cout << line << "奖品信息" << line << endl;
-    for(i=0;i<num;i++){
+    for(i=0;i<n_t;i++){
         while(word!=start[0]) word = fgetc(inf_prz);
         cout <<line<<"奖品"<<i+1<<endl;
         cout << "数量：" ;
@@ -286,6 +395,57 @@ void Info_prize()
             word = fgetc(inf_prz);
             if(word==over[0]) break;
             cout << word;
+        }
+    }
+    hitback();
+    fclose(inf_prz);
+}
+
+void Info_prz_m()
+{
+    clrscr();
+    if((inf_prz = fopen("sys_inf/inf_prz_m.txt","r")) == NULL){
+        cout << "读取信息失败" << endl;
+        hitback();
+        return;
+    }
+    char word = fgetc(inf_prz);
+    int n_t = word - '0';
+    int n_p = 0;
+    int i = 0;
+    int j = 0;
+
+    cout << line << "奖品信息" << line << endl;
+    cout << "奖品组数：" << n_t << endl;
+    for(i=0;i<n_t;i++){
+        cout << line << "第" << i+1 << "组奖品："<< endl;
+        cout << "奖品种类数：" ;
+        while(word!=prz_m_s[0]) word = fgetc(inf_prz);
+        for(j=0;word!='\n';j++) {
+            word = fgetc(inf_prz);
+            answer[j] = word;
+            cout << word;
+        }
+        n_p = atoi(answer);
+        for(j=0;j<n_p;j++){
+            while(word!=start[0]) word = fgetc(inf_prz);
+            cout <<"奖品"<<j+1<<endl;
+            cout << "数量：" ;
+            while(word!='\n') {
+                word = fgetc(inf_prz);
+                cout << word;
+            }
+            word = fgetc(inf_prz);
+            cout << "名称: ";
+            while((word = fgetc(inf_prz))!='\n')
+                cout << word;
+            word = '0';
+            cout << endl <<"奖品信息：";
+            while(word!='\n') {
+                word = fgetc(inf_prz);
+                if(word==over[0]) break;
+                cout << word;
+            }
         }
     }
     hitback();
@@ -420,7 +580,6 @@ void Blank(FILE* file)
 
 void User_set()
 {
-    char answer = '0';
     while(1){
         clrscr();
         cout << line << "用户管理" << line << endl;
@@ -428,13 +587,14 @@ void User_set()
         cout << "2.删除用户" << endl;
         cout << "3.修改用户信息" << endl;
         cout << "4.返回" << endl;
-        cin >> answer;
-        switch(answer-'0'){
+        num = Input_N(answer);
+        //cin >> num;
+        switch(num){
             case 1: User_add(); break;
             case 2: User_delete(); break;
             case 3: User_info_c(); break;
             case 4: return;
-            default: answer = '0';
+            default: num = 0;break;
         }
     }
 }
@@ -442,7 +602,7 @@ void User_set()
 void User_add()
 {
     int age = 0;
-    char sex = '0';
+    int sex = 0;
     char id[id_len];
     char AGE[3];
     char name[n_name];
@@ -468,13 +628,14 @@ void User_add()
 
     while(age<=0||age>99){
         cout << "年龄(1-99)：";
-        cin >> age;
+        age = Input_N(answer);
     }
     itoa(age,AGE,10);
-    while((sex-'0')!=1&&(sex-'0')!=2){
+    while(sex!=1&&sex!=2){
         cout << "性别：1.男\t2.女" << endl;
-        cin >> sex;
+        sex = Input_N(answer);
     }
+    cout << ID <<endl;
 
     cout << "个人简介(100字节):" << endl;
     if(Input_B(info,max_n+1) == 0) return;
@@ -487,6 +648,7 @@ void User_add()
     }
     if((IDs = fopen("sys_inf/IDs.txt","a+")) == NULL){
         cout << "创建用户ID失败" << endl;
+        remove(ID);
         delete ID;
         fclose(inf_user);
         hitback();
@@ -514,7 +676,6 @@ void User_add()
 void User_delete()
 {
     clrscr();
-    char answer = '0';
     char id[id_len];
     cout << line << "删除用户" << line << endl;
     cout << "请输入要删除的用户的ID" << endl;
@@ -533,12 +694,10 @@ void User_delete()
 
     cout << "确定要删除ID：" << id <<"?"<< endl;
 
-    int in;
-    while(answer == '0'){
+    while(1){
         cout << "1.确认删除" <<endl<<"2.返回" << endl;
-        cin>>answer;
-        in = answer - '0';
-        if(in == 1){
+        num = Input_N(answer);
+        if(num == 1){
                 if(Delete_ID(id)==1){
                     remove(ID);
                     cout << "删除成功！" << endl;
@@ -546,7 +705,7 @@ void User_delete()
                     hitback();
                     return;
                 }
-        }else if(in == 2){
+        }else if(num == 2){
             delete ID;
             return;
         }
@@ -620,27 +779,25 @@ void User_info_c()
     int len = strlen(id)+strlen(path);
     char *ID = new char[len+1];
     F_Add(path,id,ID);
-    cout << ID << endl;
-    if((inf_user = fopen(ID,"r+")) == NULL){
+
+    if((inf_user = fopen(ID,"r")) == NULL){
         cout << "读取用户数据失败" << endl;
+        delete ID;
         hitback();
         return;
     }
     fclose(inf_user);
-
     while(age<=0||age>99){
         cout << "年龄(1-99)：";
-        cin >> age;
+        age = Input_N(answer);
     }
-    itoa(age-'0',AGE,10);
-    while((sex-'0')!=1&&(sex-'0')!=2){
+    itoa(age,AGE,10);
+    while(sex!=1&&sex!=2){
         cout << "性别：1.男\t2.女" << endl;
-        cin >> sex;
+        sex = Input_N(answer);
     }
-    while(1){
-        cout << "个人简介(100字节):" << endl;
-        if(Input(info,max_n+1) == 0) return;
-    }
+    cout << "个人简介(100字节):" << endl;
+    if(Input_B(info,max_n+1) == 0) return;
 
     if((inf_user = fopen(ID,"w"))==NULL){
         cout << "写入用户数据失败" << endl;
@@ -655,24 +812,26 @@ void User_info_c()
     fwrite(info,s_c,strlen(info),inf_user);
     fclose(inf_user);
     delete ID;
+    cout << "修改成功！" << endl;
+    hitback();
     return;
 }
 
 void Lottery()
 {
-    clrscr();
-    cout << line <<"抽奖"<< line <<endl;
-    cout << "1.单组抽奖" <<endl;
-    cout << "2.多组抽奖" <<endl;
-    cout << "3.返回" << endl;
-    char answer = '0';
-    while(answer=='0'){
-        cin >> answer;
-        switch(answer-'0'){
+    while(1){
+        clrscr();
+        cout << line <<"抽奖"<< line <<endl;
+        cout << "1.单组抽奖" <<endl;
+        cout << "2.多组抽奖" <<endl;
+        cout << "3.返回" << endl;
+
+        num = Input_N(answer);
+        switch(num){
             case 1: Single(); break;
 //            case 2: Multi(); break;
             case 3: return;
-            default : answer = '0';
+            default : num = 0; break;
         }
     }
 }
@@ -864,7 +1023,9 @@ int Input(char* input, int len)
         illegal = 0;
         cin.sync();     //清空缓冲区
         cin.get(input,len);
+        //if(input[0] == '/n') return 0;
         l = strlen(input);
+        //if(l<=0) return 0;
         for(i=0;i<l;i++){
             //if(input[i] == 27) return 0;
             if(Illegal_word(input[i]) == 1) illegal = 1;
@@ -887,12 +1048,28 @@ int Input_B(char* input, int len)
         illegal = 0;
         cin.sync();     //清空缓冲区
         cin.get(input,len);
+        //if(input[0] == '\n') return 0;
         l = strlen(input);
+        //if(l<=0) return 0;
         for(i=0;i<l;i++){
-            //if(input[i] == 27) return 0;
+            if(input[i] == 27) return 0;
             if(Illegal_word(input[i]) == 1) illegal = 1;
         }
         if(illegal == 1) cout << "请重新输入: ";
         else return 1;
     }
+    return 1;
+}
+
+int Input_N(char *input)
+{
+    int num = 0;
+    int i = 0;
+    for(i=0;i<2;i++) input[i] = '\0';
+    while(num == 0){
+        cin.sync();
+        cin >> input;
+        num = atoi(input);
+    }
+    return num;
 }
